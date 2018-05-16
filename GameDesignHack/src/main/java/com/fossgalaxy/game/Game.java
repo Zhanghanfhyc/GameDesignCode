31c06d3e61dd2fefefc3a2eca80f3b02e8164203
LAPTOP-1J3G52L7\Ren Yifan <renyifan008@gmail.com>
2018-05-16 20:10:27 +0100

    Update again

--- GameDesignHack/src/main/java/com/fossgalaxy/game/Game.java
+++ GameDesignHack/src/main/java/com/fossgalaxy/game/Game.java
@@ -6,11 +6,8 @@ import com.fossgalaxy.games.tbs.io.SettingsIO;
 public class Game {
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
         App.run(SettingsIO.buildWithExtras("com.fossgalaxy.game"), "game.json", "map_test.json", "Player", "Player","Player");
-=======
-        App.run(SettingsIO.buildWithExtras("com.fossgalaxy.game"), "game.json", "map_14X7_horizental_with_forest.json", "Player", "Player","Player");
->>>>>>> 3621324ea87d7c023435666a8d774f1973e33b07
+
 
     }
 
